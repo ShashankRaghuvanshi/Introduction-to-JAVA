15ebc89312d38553b4d2a35a1279061a687159e6
Shashank Raghuvanshi <ishashankraghuvanshi@gmail.com>
2024-02-25 16:38:16 +0530

    Create Welcome_ninjas.java

---
+++ Introduction_to_JAVA/src/Lecture_2_Getting_started_with_JAVA/Welcome_ninjas.java
@@ -0,0 +1,10 @@
+package Lecture_2_Getting_started_with_JAVA;
+
+public class Welcome_ninjas {
+    public static void printDisplayMessage() 
+    {
+        System.out.println("Welcome to Java course!");
+       System.out.println("Hello");
+       System.out.println("Ninja");
+    }
+}
